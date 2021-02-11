b47d26fae72a6bec77bf20c92493f0b60007181c
Thibaud Launay <thibaudlaunay@hotmail.fr>
2021-02-11 17:25:37 +0100

    Moving Thibaud-Changes to the main

---
+++ Git-Unity-ex/Assets/Scripts/CubeMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("z"))
+            transform.Translate(Vector3.forward);
+        if (Input.GetKeyDown("s"))
+            transform.Translate(-Vector3.forward);
+        if (Input.GetKeyDown("q"))
+            transform.Translate(Vector3.left);
+        if (Input.GetKeyDown("d"))
+            transform.Translate(Vector3.right);
+    }
+}
